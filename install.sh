7c4cd4e2c2629ccb7b05d206b82f239fbf101338
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-04-28 12:47:51 +0900

    Create symbolic links

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# install oh-my-zsh
+SH=~/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git "$ZSH"
+ln -sf $HOME/dotfiles/zshrc $HOME/.zshrc
+
+# set up vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -sf $HOME/dotfiles/vimrc $HOME/.vimrc
