62dfbb532fcd63cf51f54a10346092300d50e9a0
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-05-11 11:09:00 +0900

    Fix wrong destination parameter

--- install.sh
+++ install.sh
@@ -17,4 +17,4 @@ done
 brew cask install visual-studio-code
 
 # install code command
-ln -s /usr/local/bin/code "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+ln -s "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" /usr/local/bin/code
